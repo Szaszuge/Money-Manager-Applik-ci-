4c8cf0f74e003f60eb353d918178f086c08bca67
Szaszuge <78954822+Szaszuge@users.noreply.github.com>
2023-11-15 10:28:11 +0100

    adatfelvitel

--- StepcounterApp/Controllers/newdata.js
+++ StepcounterApp/Controllers/newdata.js
@@ -10,46 +10,31 @@ function getToday(){
 function addSteps(){
 
     let date = document.querySelector('#date');
-    let steps = document.querySelector('#steps');
+    let type = document.querySelector('#type');
+    let tag = document.querySelector('#tag');
+    let amount = document.querySelector('#amount');
 
-    if (date.value == "" || steps.value == 0 ){
+    if (date.value == "" || tag.value == "" || amount.value == 0){
         showMessage("Nem adtál meg minden adatot!");
     }
     else{
-        axios.get(`${serverURL}/steps/userID/eq/${loggedUser.ID}`).then(res=>{
-            let vane = false;
-            let upID = -1;
-            res.data.forEach(item => {
-                if (item.date.split('T')[0] == date.value){
-                    vane = true;
-                    upID = item.ID;
-                    return;
-                }
-            });
-            if(vane){
+ 
                 let data = {
-                    steps : steps.value	
-                }
-                axios.patch(`${serverURL}/steps/ID/eq/${upID}`, data).then((res)=>{
-                    alert('A lépésszám módosítva!');
-                    date.value = null;
-                    steps.value = 0;
-                });
-            }
-            else{
-                let data = {
-                    userID : loggedUser.ID,	
-                    date : date.value,	
-                    steps : steps.value	
+                    date: date.value,
+                    type : type.value,
+                    tag : tag.value,
+                    amount : amount.value	
                 }
 
-                axios.post(`${serverURL}/steps`, data).then((res)=>{
-                    alert('A lépésszám rögzítve!');
+                axios.post(`${serverURL}/items`, data).then((res)=>{
+                    alert('A tétel rögzítve!');
                     date.value = null;
-                    steps.value = 0;
+                    type.value = "kiadas";
+                    tag.value = "";
+                    amount.value = 0;
                 });
-            }
-        })
+         
+       
     }
 }
 
